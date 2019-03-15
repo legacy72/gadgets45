e97b91ed4e35df8bed47f8ab28fe38a5af4d1dd6
briksing <briksing@yandex.ru>
2019-03-15 16:52:31 +0500

    Remove carousel form catalog page. Add collapse for left menu.

--- catalog.php
+++ catalog.php
@@ -12,6 +12,7 @@ $images = getImages($dbh);
 	<link rel="stylesheet" type="text/css" href="styles/styles.css">
 	<link rel="stylesheet" type="text/css" href="slick-master/slick/slick-theme.css"/>
 	<link rel="stylesheet" type="text/css" href="slick-master/slick/slick.css"/>
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
 </head>
 <body>
 	<header>
@@ -103,7 +104,111 @@ $images = getImages($dbh);
 					Каталог
 				</div>
 				<div class="catalog_container">
-					<div class="catalog_menu">
+
+					<div class="accordion catalog_menu" id="accordionExample">
+					  <div class="card">
+					    <div class="card-header" id="headingOne">
+					      <h2 class="mb-0">
+					        <div class="header-menu-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+					          Цена
+					        </div>
+					      </h2>
+					    </div>
+
+					    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+					      <div class="card-body">
+									<div class="price_range">
+
+										<div class="price_range__label">
+											<label class="label-price-form" for="price_from">От</label>
+											<label class="label-price-to" for="price_to">До</label>
+										</div>
+										<div class="price_range__inputs">
+											<input class="price_from" id="price_from" type="text" placeholder="6990" name="">
+											<input class="price_to" id="price_to" type="text" placeholder="199 000" name="">
+										</div>
+
+									</div>
+									<div class="prices-range__checkbox">
+										<div class="price_variants_row">
+											<input type="checkbox" name="">
+											<span class="price-variants-row__range">20 000 - 30 000 р.</span>
+										</div>
+										<div class="price_variants_row">
+											<input type="checkbox" name="">
+											<span class="price-variants-row__range">30 000 - 40 000 р.</span>
+										</div>
+										<div class="price_variants_row">
+											<input type="checkbox" name="">
+											<span class="price-variants-row__range">40 000 - 50 000 р.</span>
+										</div>
+										<div class="price_variants_row">
+											<input type="checkbox" name="">
+											<span class="price-variants-row__range">50 000 - 60 000 р.</span>
+										</div>
+										<div class="price_variants_row">
+											<input type="checkbox" name="">
+											<span class="price-variants-row__range">60 000 - 70 000 р.</span>
+										</div>
+										<div class="price_variants_row">
+											<input type="checkbox" name="">
+											<span class="price-variants-row__range">70 000 - 80 000 р.</span>
+										</div><div class="price_variants_row">
+											<input type="checkbox" name="">
+											<span class="price-variants-row__range">80 000 - 90 000 р.</span>
+										</div>
+									</div>
+					      </div>
+					    </div>
+					  </div>
+
+					  <div class="card">
+					    <div class="card-header" id="headingTwo">
+					      <h2 class="mb-0">
+					        <div class="header-menu-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+					          Диагональ
+					        </div>
+					      </h2>
+					    </div>
+					    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+					      <div class="card-body">
+					        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+					      </div>
+					    </div>
+					  </div>
+
+					  <div class="card">
+					    <div class="card-header" id="headingThree">
+					      <h2 class="mb-0">
+					        <div class="header-menu-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+					          Бренд
+					        </div>
+								</h2>
+					    </div>
+					    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+					      <div class="card-body">
+					        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+					      </div>
+					    </div>
+					  </div>
+
+						<div class="card">
+							<div class="card-header" id="headingFor">
+								<h2 class="mb-0">
+									<div class="header-menu-link collapsed" data-toggle="collapse" data-target="#collapseFor" aria-expanded="false" aria-controls="collapseFor">
+										Оперативная память
+									</div>
+								</h2>
+							</div>
+							<div id="collapseFor" class="collapse" aria-labelledby="headingFor" data-parent="#accordionExample">
+								<div class="card-body">
+									Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+								</div>
+							</div>
+						</div>
+
+					</div>
+					<!-- <div class="catalog_menu">
 						<div class="catalog_price_title">
 							Цена
 						</div>
@@ -134,7 +239,7 @@ $images = getImages($dbh);
 								<input type="checkbox" name=""><p>60 000 р. и более</p>
 							</div>
 						</div>
-					</div>
+					</div> -->
 					<div class="catalog_block">
 						<div class="catalog_header">
 							<div class="catalog_title">
@@ -149,13 +254,11 @@ $images = getImages($dbh);
 								</div>
 							</div>
 						</div>
-						<div class="slider">
-							<div class="phone-carousel">
+							<div class="catalog_items">
 								<?php foreach($images as $image): ?>
 									<div class="catalog_item"><img src=<?=PRODUCT_IMAGES_PATH.$image['name']; ?>></div>
 								<?php endforeach; ?>
 							</div>
-						</div>
 					</div>
 				</div>
 			</div>
@@ -195,8 +298,5 @@ $images = getImages($dbh);
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 	<script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
 
-	<!-- Slick-Carousel -->
-	<script type="text/javascript" src="scripts/catalog-carousel.js" charset="utf-8"></script>
-	<script type="text/javascript" src="slick-master/slick/slick.min.js"></script>
 </body>
 </html>
